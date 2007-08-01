770a082f5f93f558620a5eaadf63f075f6b894bd
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2007-08-01 16:41:51 +0000

    new structure with singleton class methods
    git-svn-id: http://floe.googlecode.com/svn/trunk@16 bff8106b-f335-0410-b21d-6d730982856d

--- lib/floe.rb
+++ lib/floe.rb
@@ -1,38 +1,44 @@
 require 'rubygems'
 require 'rake'
-require 'commandline'
+
+#unless defined? $lang
+#  require 'floe/en'
+#end
 
 module Floe
 
-  class Application < CommandLine::Application
-    def initialize
-      author    "Mark Rickerby"
-      copyright "Coretxt, 2007"
-      synopsis  "{options} <task> [context]"
-      short_description "minimalist tool that flattens the effort of building web applications"
-      long_description  "minimalist tool that flattens the effort of building web applications"
-      options :help, :debug
-      option  :names => "--in-file", :opt_found => get_args,
-              :opt_description => "Input file for sample app.",
-              :arg_description => "input_file"
-      expected_args :file
-    end
-  
-    def main
-      puts "args:      #{args}"
-      puts "--in-file: #{opt["--in-file"]}"
-    end
-    
-    def abort_with_message(msg)
-      puts "Floe aborted: " + msg
-    end
-    
-    def run(command)
+  class Application
+    def self.run(cmd, args='')
       begin
-        Rake::Task[command].invoke
-      rescue Exception => err
-        abort_with_message(err)
-       end
+        CommandIndex.send(cmd, args)
+        true
+      rescue Exception => e
+        puts e
+        false
+      end
+    end
+  end
+  
+  class CommandIndex
+    
+    def self.status(event)
+      event
+    end
+    
+    def self.todo(event)
+      event
+    end
+    
+    def self.configure(event)
+      event
+    end
+
+    def self.install(event)
+      event
+    end    
+    
+    def self.method_missing(method, args)
+      puts UNKNOWN_COMMAND
     end
   end
 
