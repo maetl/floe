3a8b9a0f67a91956fcf932b61040362219c1a2dd
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2007-08-06 13:10:19 +0000

    Tidying up the language.Inflector API and implementing a more flexible set of rule applications
    git-svn-id: http://floe.googlecode.com/svn/trunk@23 bff8106b-f335-0410-b21d-6d730982856d

---
+++ ext/php/classes/language/Inflections.class.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package language
+ */
+
+/**
+ * @package language
+ */
+interface Inflections {
+
+	/**
+	 * @param $word string
+	 */
+	static function toIdentifier($word);
+	
+	/**
+	 * @param $word string
+	 */
+	static function toWords($word);
+
+	/**
+	 * @param $word string
+	 */
+	static function toSentence($word);
+	
+	/**
+	 * @param $word string
+	 */
+	static function toPlural($word);
+	
+	/**
+	 * @param $word string
+	 */
+	static function toSingular($word);
+	
+	/**
+	 * @param $number numeric
+	 */
+	static function toOrdinal($number);
+	
+}
+
+?>
--- ext/php/classes/language/en/Inflector.class.php
+++ ext/php/classes/language/en/Inflector.class.php
@@ -1,5 +1,130 @@
 <?php
+/**
+ * @package language
+ * @subpackage en
+ */
+require_once 'language/Inflections.class.php';
 
+/**
+ * Performs linguistic transformations based on standard English
+ * grammar rules.
+ * 
+ * Note that this class implements the language.Inflect interface
+ * 
+ * @package language
+ * @subpackage en
+ */
+class Inflect implements Inflections {
+
+	/**
+	 * @param $word string
+	 */
+	static function toIdentifier($word) {
+		return str_replace(" ","", ucwords(str_replace("_"," ",$word)));
+	}
+	
+	/**
+	 * @param $word string
+	 */
+	static function toWords($word) {
+	
+	}
+
+	/**
+	 * @param $word string
+	 */
+	static function toSentence($word) {
+	
+	}
+	
+	/**
+	 * map of regular plural rules
+	 */
+	static private $RegularPluralRules = array(
+		'/(x|ch|ss|sh)$/' => '\1es',
+		'/([^aeiouy]|qu)y$/' => '\1ies',
+		'/([o])$/' => '\1es',
+		'/(?:([^f])fe|([lr])f)$/' => '\1\2ves',
+		'/man$/' => '\1men',
+		'/$/' => 's'
+	);
+			
+	/**
+	 * map of irregular plural rules
+	 */
+	static private $IrregularPluralRules = array(
+		'person' => 'people',
+		'child' => 'children',
+		'octopus' => 'octopi',
+		'movie' => 'movies',
+		'news' => 'news',
+		'status' => 'status',
+		'series' => 'series'
+	);
+		
+	/**
+	 * map of singular rules
+	 */
+	static private $SingularRules = array(
+		'/(x|ch|ss)es$/' => '\1',
+		'/([^aeiouy]|qu)ies$/' => '\1y',
+		'/([lr])ves$/' => '\1f',
+		'/([^f])ves$/' => '\1fe',
+		'/(analy|ba|diagno|parenthe|progno|synop|the)ses$/' => '\1sis',
+		'/([ti])a$/' => '\1um',
+		'/men$/' => '\1man',
+		'/s$/' => ''
+	);
+	
+	/**
+	 * Converts a singular word to plural form.
+	 * 
+	 * @param $word string
+	 */
+	static function toPlural($word) {
+		if (array_key_exists($word, Inflect::$IrregularPluralRules)) {
+			return Inflect::$IrregularPluralRules[$word];
+		}
+		return ($result = Inflect::applyRules($word, Inflect::$RegularPluralRules)) ? $result : $word;
+	}
+	
+	/**
+	 * Converts a plural word to singular form.
+	 * 
+	 * @param $word string
+	 */
+	static function toSingular($word) {
+		if ($key = array_search($word, Inflect::$IrregularPluralRules)) {
+			return $key;
+		}
+		return ($result = Inflect::applyRules($word, Inflect::$SingularRules)) ? $result : $word;
+	}
+	
+	/**
+	 * @param $number numeric
+	 */
+	static function toOrdinal($number) {
+	
+	}
+	
+	/**
+	 * Applies a map of grammar rules to match a given word.
+	 * 
+	 * Returns the transformed word, or false if the word was not matched.
+	 * 
+	 * @param $word string
+	 * @param $rules array of grammar rules
+	 */
+	static private function applyRules($word, $rules) {
+		foreach($rules as $rule => $replace) {
+			if (preg_match($rule, $word)) {
+				return preg_replace($rule, $replace, $word);
+			}
+		}
+		return false;
+	}
+	
+}
 	/**
 	 * @todo normalize naming convention
 	 * @package language
