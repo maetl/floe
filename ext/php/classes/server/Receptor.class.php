e937b0f5f460a4a383079e98a6fc75f4816a6644
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2007-08-15 14:01:15 +0000

    servlet style interface for server filters
    git-svn-id: http://floe.googlecode.com/svn/trunk@34 bff8106b-f335-0410-b21d-6d730982856d

---
+++ ext/php/classes/server/Receptor.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* @package server
+*/
+
+/**
+* Represents a node in the server filter chain.
+* 
+* Receptors are used by the cell membrane to dispatch requests
+* to internal controllers. Uses the servlet model of feeding
+* a request and response object to the process run method.
+* 
+* @package server
+* @abstract
+*/
+interface Receptor {
+    /**
+    * @param request request object
+    * @param response response object
+	* @abstract
+    */
+    public function run(Request $request, Response $response);
+
+}
+
+?>
