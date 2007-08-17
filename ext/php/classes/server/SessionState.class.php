f5aaeb09eb85ab1b86fdaaf4eabd1e325f883792
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2007-08-17 22:06:18 +0000

    git-svn-id: http://floe.googlecode.com/svn/trunk@44 bff8106b-f335-0410-b21d-6d730982856d

---
+++ ext/php/classes/server/SessionState.class.php
@@ -0,0 +1,91 @@
+<?php
+// $Id: Session.class.php 52 2007-05-06 08:36:41Z maetl_ $
+/**
+ * @package server
+ */
+
+/**
+ * Currently just wraps the session array provided by PHP. This is old code
+ * and badly implemented, but there is ample room for improvement.
+ * 
+ * This session behaves like a hash map that reloads itself when the constructor
+ * is called. Values must be explicitly cleared, or else they will persist until
+ * the current browser session ends. The whole session can be cleared using the
+ * destroy method.
+ * 
+ * @package server
+ * @todo remove duplication in data members
+ * @todo encapsulate session storage as a static var 
+ * @todo gut the class and add better support for persistent storage
+ * @todo add explicit timeout control
+ * @todo add multiple subdomain control
+ */
+class SessionState {
+	private $_properties;
+	private $_id;
+	
+	/**
+	 * Create a singleton instance of the class.
+	 */
+	static function instance() {
+		if (!isset($GLOBALS['Session'])) {
+			$GLOBALS['Session'] = new SessionState();
+		}
+		return $GLOBALS['Session'];
+	}
+	
+	function __construct() {
+		@session_start();
+		$this->_properties = array();
+		foreach($_SESSION as $key=>$value) {
+			$this->_properties[$key] = $value;
+		}
+	}
+	
+	
+	/**
+	 * Add a new key=>value pair to the session.
+	 * 
+	 * If the key already exists, the value is overwritten.
+	 */
+	function set($key, $value) {
+		$_SESSION[$key] = $value;
+		$this->_properties[$key] = $value;
+	}
+	
+	/**
+	 * Get the value of a key.
+	 * 
+	 * Returns false if the key does not exist.
+	 */
+	function get($key) {
+		return (isset($this->_properties[$key])) ? $this->_properties[$key] : false;
+	}
+	
+	/**
+	 * Remove a key=>value pair from the session.
+	 */
+	function remove($key) {
+		unset($_SESSION[$key]);
+		unset($this->_properties[$key]);
+	}
+	
+	/**
+	 * Clear all key=>value pairs from the session
+	 */
+	function destroy() {
+		session_destroy();
+		unset($this->_properties);
+	}
+
+	function __get($key) {
+		return $this->get($key);
+	}
+	
+	function __set($key, $value) {
+		return $this->set($key, $value);
+	}
+	
+}
+
+?>
---
+++ ext/php/classes/server/controllers/SessionController.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package server
+ * @subpackage controllers
+ */
+require_once 'server/SessionState.class.php';
+
+/** 
+ * Provides session state to the controller subclass.
+ * 
+ * @package server
+ * @subpackage controllers
+ */
+class BaseController {
+	protected $request;
+	protected $response;
+	protected $session;
+
+	public function __construct($request, $response) {
+		$this->request = $request;
+		$this->response = $response;
+		$this->session = SessionState::instance();
+	}
+
+}
+
+
+?>
