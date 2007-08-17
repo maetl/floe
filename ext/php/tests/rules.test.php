115d938e4ac88d11a9bf5ed346ed322d3b6a0c9d
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2007-08-17 22:06:53 +0000

    git-svn-id: http://floe.googlecode.com/svn/trunk@45 bff8106b-f335-0410-b21d-6d730982856d

---
+++ ext/php/tests/rules.test.php
@@ -0,0 +1,87 @@
+<?php
+require_once 'simpletest/autorun.php';
+
+require_once 'repository/rules/EmailRule.class.php';
+require_once 'repository/rules/AlphanumericRule.class.php';
+require_once 'repository/rules/MatchingRule.class.php';
+require_once 'repository/rules/NumericRule.class.php';
+
+class EmailRuleTest extends UnitTestCase {
+
+	function testAcceptsValidEmails() {
+		$rule = new EmailRule();
+		$this->assertTrue($rule->validate("you@example.com"));
+		$this->assertTrue($rule->validate("first.last@example.com"));
+		//$this->assertTrue($rule->validate("you+sub@example.com")); // currently unsupported
+	}
+
+	function testRejectsInvalidEmails() {
+		$rule = new EmailRule();
+		$this->assertFalse($rule->validate("you@example"));
+		$this->assertFalse($rule->validate("first.com"));
+		$this->assertFalse($rule->validate("@"));
+		$this->assertFalse($rule->validate("x@not.a.valid.address"));
+	}
+	
+}
+
+class AlphanumericRuleTest extends UnitTestCase {
+
+	function testAcceptsOnlyAlphaChars() {
+		$rule = new AlphanumericRule();
+		$this->assertTrue($rule->validate("abcde"));
+		$this->assertTrue($rule->validate("abcde123"));
+		$this->assertTrue($rule->validate("1234"));
+	}
+	
+	function testRejectsNonAlphaChars() {
+		$rule = new AlphanumericRule();
+		$this->assertFalse($rule->validate("*@#$"));
+		$this->assertFalse($rule->validate(""));
+		$this->assertFalse($rule->validate("..."));
+	}
+
+}
+
+class MatchingRuleTest extends UnitTestCase {
+
+	function testAcceptsMatching() {
+		$rule = new MatchingRule("hello");
+		$this->assertTrue($rule->validate("hello"));
+		$rule = new MatchingRule(1);
+		$this->assertTrue($rule->validate(1));
+	}
+	
+	function testRejectsNonMatching() {
+		$rule = new MatchingRule("hello");
+		$this->assertFalse($rule->validate("world"));
+		$rule = new MatchingRule(1);
+		$this->assertFalse($rule->validate(2));
+		// empty values are considered invalid
+		$rule = new MatchingRule("");
+		$this->assertFalse($rule->validate(""));
+	}
+
+}
+
+class NumericRuleTest extends UnitTestCase {
+
+	function testAcceptsAllNumericTypes() {
+		$rule = new NumericRule();
+		$this->assertTrue($rule->validate(1));
+		$this->assertTrue($rule->validate(2.0));
+		$this->assertTrue($rule->validate(9999));
+		$this->assertTrue($rule->validate("10"));
+		$this->assertTrue($rule->validate(pi()));
+	}
+
+	function testRejectsAllNonNumericTypes() {
+		$rule = new NumericRule();
+		$this->assertFalse($rule->validate("hello"));
+		$this->assertFalse($rule->validate(new stdClass));
+		$this->assertFalse($rule->validate(array(1)));
+	}
+	
+}
+
+?>
