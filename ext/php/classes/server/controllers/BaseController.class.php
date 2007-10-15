3256fabad860d0c6fe127f42df73120a79d8e5cd
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2007-10-15 08:50:02 +0000

    adding method to auto import matching model class when controller is created
    git-svn-id: http://floe.googlecode.com/svn/trunk@100 bff8106b-f335-0410-b21d-6d730982856d

--- ext/php/classes/server/controllers/BaseController.class.php
+++ ext/php/classes/server/controllers/BaseController.class.php
@@ -19,6 +19,24 @@ class BaseController {
 	public function __construct($request, $response) {
 		$this->request = $request;
 		$this->response = $response;
+		$this->importModel();
+	}
+	
+	/**
+	 * Imports a model file based on controller identity.
+	 * 
+	 * Should this be phased out in favor of autoload?
+	 */
+	private function importModel() {
+		$modelPath = MOD_DIR . '/' . $this->identity() . '.model.php';
+		if (file_exists($modelPath)) require_once $modelPath;
+	}
+	
+	/**
+	 * The identity of this controller.
+	 */
+	public function identity() {
+		return strtolower(str_replace('Controller', '', get_class($this)));
 	}
 
 }
