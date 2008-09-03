304e904d48e877fde7b61131d1ef962da21a39f4
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2008-09-03 01:13:50 +0000

    fixing response test to work via cli and adding additional tests for template response buffer
    git-svn-id: http://floe.googlecode.com/svn/trunk@181 bff8106b-f335-0410-b21d-6d730982856d

---
+++ dev/tests/resources/templates/vars.php
@@ -0,0 +1,7 @@
+<ul>
+	<li>booleanVar: <?php echo $booleanVar; ?></li>
+	<li>integerVar: <?php echo $integerVar; ?></li>
+	<li>stringVar: <?php echo $stringVar; ?></li>
+	<li>arrayVar: <?php echo implode(',', $arrayVar); ?></li>
+	<li>objectVar: <?php echo $objectVar->var; ?></li>
+</ul>
--- dev/tests/response.test.php
+++ dev/tests/response.test.php
@@ -1,6 +1,13 @@
 <?php
 require_once 'simpletest/autorun.php';
+
+/**
+ * Note: some of these tests won't work when the Response is triggered via CLI
+ * rather than via Apache.
+ */
 require_once dirname(__FILE__).'/../../src/server/Response.class.php';
+
+if (!defined('TPL_DIR')) define('TPL_DIR', dirname(__FILE__).'/resources/templates/');
 
 class ResponseTest extends UnitTestCase {
 	
@@ -9,10 +16,10 @@ class ResponseTest extends UnitTestCase {
 	}
 	
 	function testHeadersAlreadySent() {
-		//$this->assertResponseHeader("Content-Type");
 		$response = new Response();
 		$response->header("X-Non-Authenticate", "Negotiate");
-		//$this->expectException();
+		$this->assertResponseHeader("X-Non-Authenticate");
+		$this->expectException('Exception');
 		$response->out();
 	}
 	
@@ -20,10 +27,33 @@ class ResponseTest extends UnitTestCase {
 		$response = new Response();
 		$response->write("<h1>Hello</h1>");
 		$response->write("<p>World</p>");
-		$this->expectException();
-		$response->out();
-		$content = ob_get_contents();
-		$this->assertPattern("/<h1>Hello<\/h1><p>World<\/p>/", $content);
+		$this->assertPattern("/<h1>Hello<\/h1><p>World<\/p>/", $response->body());
+	}
+	
+	function testTemplateRenderPlain() {
+		$response = new Response();
+		$response->render('hello');
+		$this->assertPattern("/<h1>Hello World<\/h1>/", $response->body());
+	}
+	
+	function testTemplateRenderVars() {
+		$response = new Response();
+		$response->assign('booleanVar', true);
+		$response->assign('integerVar', 999);
+		$response->assign('stringVar', 'sesame');
+		$response->assign('arrayVar', array("green","eggs","boiled","ham"));
+		$object = new stdClass; $object->var = "value";
+		$response->assign('objectVar', $object);
+		$response->render('vars');
+		$this->assertPattern("/<li>booleanVar: 1<\/li>/", $response->body());
+		$this->assertPattern("/<li>integerVar: 999<\/li>/", $response->body());
+		$this->assertPattern("/<li>stringVar: sesame<\/li>/", $response->body());
+		$this->assertPattern("/<li>arrayVar: green,eggs,boiled,ham<\/li>/", $response->body());
+		$this->assertPattern("/<li>objectVar: value<\/li>/", $response->body());
+	}
+	
+	function testTemplateRenderWrappedLayout() {
+		
 	}
 	
 }
