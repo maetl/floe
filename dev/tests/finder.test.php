b10b109a9f0f3fc9993d2b052ffa404aff59c140
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2008-09-22 04:08:26 +0000

    adding dynamic finder methods
    git-svn-id: http://floe.googlecode.com/svn/trunk@199 bff8106b-f335-0410-b21d-6d730982856d

--- dev/tests/finder.test.php
+++ dev/tests/finder.test.php
@@ -2,6 +2,7 @@
 require_once 'simpletest/autorun.php';
 require_once 'simpletest/mock_objects.php';
 require_once dirname(__FILE__).'/../../src/repository/Finder.class.php';
+require_once dirname(__FILE__).'/../../src/repository/store/mysql/MysqlQuery.class.php';
 
 if (!defined('DB_HOST')) {
 	define('DB_HOST', 'localhost');
@@ -17,6 +18,7 @@ class Person extends Record {
 	function __define() {
 		$this->property("name", "string");
 		$this->property("age", "integer");
+		$this->property("gender", "string");
 	}
 	
 }
@@ -27,16 +29,29 @@ class FinderBasicQueryTest extends UnitTestCase {
 		$db = StorageAdaptor::instance();
 		$person = new Person();
 		$db->createTable("people", $person->properties());
-		$db->insert("people", array("name"=>"Mark", "age"=>29));
-		$db->insert("people", array("name"=>"Maxwell", "age"=>29));
-		$db->insert("people", array("name"=>"Maetl", "age"=>99));
+		$db->insert("people", array("name"=>"Jack", "age"=>29, "gender"=>"male"));
+		$db->insert("people", array("name"=>"Jill", "age"=>29, "gender"=>"female"));
+		$db->insert("people", array("name"=>"Box", "age"=>99, "gender"=>"neuter"));
 	}
 	
-	function testCollection() {
+	function testFindQueries() {
 		$finder = new PeopleFinder();
 		$this->assertEqual(count($finder->findAll()), 3);
 		$this->assertEqual(count($finder->findByKey("age", 29)), 2);
+		$query = new MysqlQuery();
+		$query->whereNotEquals("age", 99);
+		$this->assertEqual(count($finder->findByCriteria($query)), 2);
 		$this->assertIsA($finder->findById(1), 'Person');
+	}
+	
+	function testDynamicFindMethods() {
+		$finder = new PeopleFinder();
+		$person1 = $finder->findByName('Jack');
+		$this->assertEqual(29, $person1->age);
+		$person2 = $finder->findByNameAndAge("Box", 99);
+		$this->assertEqual("neuter", $person2->gender);
+		$person3 = $finder->findByNameAndAgeAndGender("Jill", 29, "female");
+		$this->assertEqual("female", $person3->gender);
 	}
 
 	function tearDown() {
--- src/repository/Finder.class.php
+++ src/repository/Finder.class.php
@@ -2,38 +2,96 @@
 require_once 'Record.class.php';
 
 /**
- * Base class for collection mappers.
+ * Base class for collection finders.
  */
 class Finder {
 	
-	private $storage;
-	private $tableName;
+	protected $storage;
+	protected $cache;
+	protected $record;
+	protected $tableName;
 	
-	protected $returnRecords = true;
-	
-	function __construct() {
+	function __construct($store=false, $cache=false) {
 		$this->storage = StorageAdaptor::instance();
-		$this->tableName = strtolower(str_replace('Finder', '', get_class($this)));
+		$collectionName = str_replace('Finder', '', get_class($this));
+		$recordName = Inflect::toSingular($collectionName);
+		$this->record = new $recordName;
+		$this->tableName = strtolower($collectionName);
 	}
-	
+
+	/**
+	 * Find a single record based on given primary key id.
+	 *
+	 * @param integer $id
+	 * @return Record
+	 */	
 	function findById($id) {
 		$this->storage->selectById($this->tableName, $id);
 		return $this->storage->getRecord();
 	}
 	
+	/**
+	 * Find records by a key=value comparison.
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 * @return array<Record>
+	 */
 	function findByKey($key, $value) {
 		$this->storage->selectByKey($this->tableName, array($key=>$value));
 		return $this->storage->getRecords();
 	}
 	
+	/**
+	 * Return all existing records.
+	 *
+	 * @return array<Record>
+	 */
 	function findAll() {
 		$this->storage->selectAll($this->tableName);
 		return $this->storage->getRecords();
 	}
 	
+	/**
+	 * Return a list of records filtered by given criteria.
+	 *
+	 * @param Query $query
+	 * @return array<Record>
+	 */
 	function findByCriteria($query) {
-		$this->storage->selectAll($this->tableName, $query);
-		return $this->storage->getRecords();	
+		$this->storage->select($this->tableName, $query->toSql());
+		return $this->storage->getRecords();
+	}
+	
+	/**
+	 * Meta-find method.
+	 *
+	 * This will generate a dynamic query based on properties of the wrapped
+	 * record schema.
+	 *
+	 * Not recommended to rely on this for production usage, as it involves a much slower
+	 * method dispatch than simply implementing a concrete find method.
+	 *
+	 * Select on multiple fields by using 'And' in the camelCase format.
+	 * 
+	 * eg: 
+	 *     $finder->findByTitle($title);
+	 *     $finder->findByFirstNameAndLastName($firstName);
+	 */
+	function __call($method, $params) {
+		if (!strstr($method, 'findBy')) throw new Exception("Method $method doesn't exist");
+		$properties = $this->record->properties();
+		$propertyName = str_replace('findBy', '', $method);
+		$requestedProperties = explode('And', $propertyName);
+		if (count($requestedProperties) != count($params)) throw new Exception("Wrong parameter count for $method");
+		$query = new MysqlQuery();
+		$count = 0;
+		foreach($requestedProperties as $property) {
+			$query->whereEquals($property, $params[$count]);
+			$count++;
+		}
+		$this->storage->select($this->tableName, $query->toSql());
+		return $this->storage->getRecord();
 	}
 	
 }
