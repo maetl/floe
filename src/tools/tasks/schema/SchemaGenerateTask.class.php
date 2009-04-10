e078e3b210240da4f27cda558b655dcc3bf7b9b6
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2009-04-10 05:29:00 +0000

    adding schema generation task
    git-svn-id: http://floe.googlecode.com/svn/trunk@275 bff8106b-f335-0410-b21d-6d730982856d

--- src/tools/tasks/schema/SchemaGenerateTask.class.php
+++ src/tools/tasks/schema/SchemaGenerateTask.class.php
@@ -28,17 +28,63 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/**
+require_once  dirname(__FILE__).'/../../../repository/store/StorageAdaptor.class.php';
+require_once dirname(__FILE__).'/../../../repository/Record.class.php';
+
+/** 
  * @package tools
  * @subpackage tasks
  */
 class SchemaGenerateTask {
 	
 	/**
-	 * @description generates a database schema from reflection on declared Record classes
+	 * @description generates a database schema from reflection on declared Record classes (warning: drops all existing tables)
 	 */
-	function process() {
-		ConsoleText::printLine("Generate a database schema");
+	function process($args) {
+		ConsoleText::printLine("Generating table schema...");
+		$db = StorageAdaptor::instance();
+		if ($db->hasTable("schema")) $db->dropTable("schema");
+		// stick all the model classes into the global namespace
+		$dir = dir(MOD_DIR);
+		while (false !== ($entry = $dir->read())) {
+			preg_match("/([a-z-]+)\.model\.php/", $entry, $matches);
+			if ($matches) {
+				require_once MOD_DIR . $entry;
+			}
+		}
+		$joinTables = array();
+		// throw up a database table for each model
+		foreach (get_declared_classes() as $class) {
+			if (is_subclass_of($class, 'Record')) {
+				$table = Inflect::toTableName($class);
+				if ($db->hasTable($table)) $db->dropTable($table);
+				$record = new $class();
+				if (get_parent_class($record) == 'Record') {
+					$db->createTable($table, $record->properties());
+					ConsoleText::printLine("created $table");
+				} else {
+					foreach($record->properties() as $name=>$type) {
+						try {
+							$db->addColumn($record->getTable(), $name, $type); 
+						} catch(ResourceError $e) {
+							// passthrough, add hasField()
+							// check to stop exception being thrown
+						}
+					}
+				}
+				foreach($record->relations() as $join) {
+					if (!array_key_exists($join, $joinTables)) {
+						$joinTables[$join] = Inflect::underscore($class)."_id";
+					} else {
+						$foreignKey = Inflect::underscore($class)."_id";
+						$fields = array($joinTables[$join]=>"integer", $foreignKey=>"integer");
+						if ($db->hasTable($join)) $db->dropTable($join);
+						$db->createTable($join, $fields);
+						ConsoleText::printLine("created $join");
+					}
+				}
+			}
+		}		
 	}
 	
 }
