19b393d94f5ad9957b6c3c4c23dea81db634932f
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2009-04-10 08:06:37 +0000

    adding rough cut of schema migration task
    git-svn-id: http://floe.googlecode.com/svn/trunk@277 bff8106b-f335-0410-b21d-6d730982856d

--- src/tools/tasks/schema/SchemaMigrateTask.class.php
+++ src/tools/tasks/schema/SchemaMigrateTask.class.php
@@ -29,16 +29,81 @@
  */
 
 /**
+ * Runs a migration script.
+ *
  * @package tools
  * @subpackage tasks
  */
 class SchemaMigrateTask {
 	
 	/**
-	 * @description performs a schema migration
+	 * @description performs a schema migration script
 	 */
-	function process() {
-		ConsoleText::printLine("Run a migration");	
+	function process($args) {
+		if (empty($args)) {
+			ConsoleText::printLine("No migration specified. Defaulting to latest version.");
+			$args[0] = 'up';
+		}
+		$db = StorageAdaptor::instance();
+		if (!$db->hasTable("schema")) {
+			echo "Creating schema table...\n";
+			$db->createTable("schema", array("version"=>"integer"));
+		}
+		
+		$db->query("select version from `schema` order by version desc");
+		$version = $db->getValue();
+		if (!$version) {
+			$db->insert("schema", array("version"=>1));
+			$version = 1;
+		}
+		
+		$versionName = str_pad($version, 3, '0', STR_PAD_LEFT);
+		$versions = $this->readMigrationDir();
+		if ($args[0] == 'up') {
+			sort($versions);
+			$offset = array_search($versionName, $versions);
+			$migrations = array_slice($versions, $offset+1);
+		} elseif ($args[0] == 'down') {
+			if (!isset($args[1])) die("No version specified for rollback.\n");
+			rsort($versions);
+			$offset = array_search($args[1], $versions);
+			$total = count($versions);
+			$migrations = array_slice($versions, 0, ($total-$offset)+1);
+		} elseif ($args[0] == 'version') {
+			echo "Schema Version $versionName\n";
+			exit;
+		} else {
+			throw new Exception("Invalid migration method. Must be up or down.");
+		}
+		foreach($migrations as $migration) {
+			$this->runMigration($migration, $args[0]);
+		}
+		$currentVersion = (int)$versions[count($versions)-1];
+		$db->update("schema", array("id"=>1), array("version"=>$currentVersion));
+	}
+	
+	function runMigration($version, $direction) {
+		require_once DEV_DIR.'/migrations/'.$version.'.migration.php';
+		$className = "Migration$version";
+		if (class_exists($className)) {
+			$migration = new $className;
+			$migration->$direction();
+			ConsoleText::printLine("Migrating $direction to $version");
+		} else {
+			throw new Exception("Migration $className not found");
+		}
+	}
+	
+	function readMigrationDir() {
+		$versions = array();
+		$dir = dir(DEV_DIR.'/migrations/');
+		while (false !== ($entry = $dir->read())) {
+			preg_match("/([0-9]+)\.migration\.php/", $entry, $matches);
+			if ($matches) {
+				$versions[] = $matches[1];
+			}
+		}
+		return $versions;
 	}
 	
 }
