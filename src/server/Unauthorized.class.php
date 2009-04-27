035aa4dbe3975d95cdc4b4e28819ec22267e2117
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2009-04-27 04:48:03 +0000

    adding HTTP 401 exception
    git-svn-id: http://floe.googlecode.com/svn/trunk@279 bff8106b-f335-0410-b21d-6d730982856d

---
+++ src/server/Unauthorized.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * $Id: Unauthorized.class.php 263 2009-03-22 06:20:18Z coretxt $
+ * @package server
+ *
+ * Copyright (c) 2007-2009 Coretxt
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * HTTP Status Code 401.
+ * 
+ * Unauthorized - Access Denied.
+ * 
+ * @package server
+ */
+require_once 'HttpError.class.php';
+
+class Unauthorized extends HttpError {
+	var $status = 401;
+	var $message = "Unauthorized";
+	var $resource;
+	var $include;
+	
+	function __construct($resource, $include) {
+		$this->resource = $resource;
+		$this->include = $include;
+	}
+	
+}
+
+?>
