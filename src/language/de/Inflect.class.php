a1ca4c41875981636012de5021a329743f414e2a
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2009-10-03 18:45:07 +0000

    adding language.de subpackage for Deutsch
    git-svn-id: http://floe.googlecode.com/svn/trunk@305 bff8106b-f335-0410-b21d-6d730982856d

---
+++ src/language/de/Inflect.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * $Id: Inflect.class.php 284 2009-05-21 23:06:07Z coretxt $
+ * @package language
+ * @subpackage de
+ */
+require_once dirname(__FILE__).'/../Inflections.class.php';
+
+/**
+ * German language support.
+ *
+ * @package language
+ * @subpackage de
+ */
+class Inflect {
+	
+}
+
+?>
