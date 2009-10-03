702de85c76cb2ba4f969d2d23b0e2f97672450c7
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2009-10-03 18:32:02 +0000

    adding custom ascii banners to console text
    git-svn-id: http://floe.googlecode.com/svn/trunk@301 bff8106b-f335-0410-b21d-6d730982856d

--- src/tools/ConsoleText.class.php
+++ src/tools/ConsoleText.class.php
@@ -1,30 +1,7 @@
 <?php
 /**
  * $Id$
- * @package language
- *
- * Copyright (c) 2007-2009 Coretxt
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+ * @package tools
  */
 
 define("BR", "\n");
@@ -39,7 +16,8 @@ define("BR", "\n");
 class ConsoleText {
 
 	function printHeader() {
-		echo self::white('Floe. '.file_get_contents(dirname(__FILE__).'/../VERSION')).BR;
+		$banner = (self::$banner) self::$banner : file_get_contents(dirname(__FILE__).'/../VERSION');
+		echo self::white('Floe. '.$banner).BR;
 	}
 
 	function printBlocks() {
@@ -85,6 +63,18 @@ class ConsoleText {
 	
 	static function endSession() {
 		if (self::$colorize) echo "\033[0m";
+	}
+
+	/** @ignore */
+	private static $banner = false;
+	
+	/**
+	 * Add a custom ASCII art banner to the console startup.
+	 *
+	 * @param string $path path to the text file containing banner to display
+	 */
+	static function banner($path) {
+		self::$banner = $path;
 	}
 	
 	/** @ignore */
