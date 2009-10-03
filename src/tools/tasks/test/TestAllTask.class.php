4e3dec863773076e57c4ecaec8d0a3cf9f80fb1f
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2009-10-03 20:37:10 +0000

    adding support for multiple reporters into test task
    git-svn-id: http://floe.googlecode.com/svn/trunk@307 bff8106b-f335-0410-b21d-6d730982856d

--- src/tools/tasks/test/TestAllTask.class.php
+++ src/tools/tasks/test/TestAllTask.class.php
@@ -5,12 +5,16 @@
  * @subpackage tasks
  */
 
+require_once 'simpletest/unit_tester.php';
+require_once 'simpletest/collector.php';
+require_once 'simpletest/xml.php';
+
 /**
  * @package tools
  * @subpackage tasks
  */
 class TestAllTask {
-	
+
 	/**
 	 * @description run all installed tests
 	 */
@@ -22,7 +26,13 @@ class TestAllTask {
 		foreach($index as $dir) {
 			if ($dir->isDir()) $test->collect($dir->getPathName(), new SimplePatternCollector('/\.test\.php$/'));
 		}
-		$test->run(new SelectiveReporter(new TextReporter, @$_SERVER['argv'][2], @$_SERVER['argv'][3]));		
+		if (isset($args[0])) {
+			$reporterName = ucfirst($args[0])."Reporter";
+			if (class_exists($reporterName)) $reporter = new $reporterName();
+		}
+		if (!isset($reporter)) $reporter = new DefaultReporter();
+		$result = $test->run($reporter);
+		return ($result ? 0 : 1);		
 	}
 	
 }
