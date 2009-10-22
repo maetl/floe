1f124f3c390d1afd66ba7aa0358bcb6342b018bb
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2009-10-22 14:09:02 +0000

    removing custom reporter from include path
    git-svn-id: http://floe.googlecode.com/svn/trunk@322 bff8106b-f335-0410-b21d-6d730982856d

--- dev/tests/floe.suite.php
+++ dev/tests/floe.suite.php
@@ -1,11 +1,9 @@
 <?php
 require_once 'simpletest/unit_tester.php';
 require_once 'simpletest/collector.php';
-require_once 'simpletest/extensions/treemap_reporter.php';
 
 $test = new TestSuite('floe.ext.php.tests');
-$fileRole = new SimplePatternCollector('/test\.php$/');
-$test->collect(dirname(__FILE__), $fileRole);
-$test->run(new TextReporter());
+$test->collect(dirname(__FILE__), new SimplePatternCollector('/test\.php$/'));
+$test->run(new DefaultReporter());
 
 ?>
