045c8efe9f3a8cc36d5d98d91e3b68b183bc9c04
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2010-02-12 14:21:55 +0000

    fixing docblocks for framework package
    git-svn-id: http://floe.googlecode.com/svn/trunk@350 bff8106b-f335-0410-b21d-6d730982856d

--- src/framework/EventLog.class.php
+++ src/framework/EventLog.class.php
@@ -1,8 +1,15 @@
 <?php
 /**
+ * This file is part of Floe, a graceful PHP framework.
+ * Copyright (C) 2005-2010 Mark Rickerby <http://maetl.net>
+ *
+ * See the LICENSE file distributed with this software for full copyright, disclaimer
+ * of liability, and the specific limitations that govern the use of this software.
+ *
  * $Id$
  * @package framework
  */
+
 require_once 'LogHandler.class.php';
 require_once 'Level.class.php';
 
