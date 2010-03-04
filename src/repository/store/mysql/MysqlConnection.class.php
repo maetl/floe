8b89211184a5d0b173a580d5d33851a0c59cbabe
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2010-03-04 16:10:03 +0000

    adding UTF8 connection by default
    git-svn-id: http://floe.googlecode.com/svn/trunk@357 bff8106b-f335-0410-b21d-6d730982856d

--- src/repository/store/mysql/MysqlConnection.class.php
+++ src/repository/store/mysql/MysqlConnection.class.php
@@ -1,32 +1,16 @@
 <?php
 /**
+ * This file is part of Floe, a graceful PHP framework.
+ * Copyright (C) 2005-2010 Mark Rickerby <http://maetl.net>
+ *
+ * See the LICENSE file distributed with this software for full copyright, disclaimer
+ * of liability, and the specific limitations that govern the use of this software.
+ *
  * $Id$
  * @package repository
  * @subpackage store.mysql
- *
- * Copyright (c) 2007-2009 Coretxt
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
  */
+ 
 require_once dirname(__FILE__) .'/../../../framework/EventLog.class.php';
 require_once dirname(__FILE__) .'/../ResourceError.class.php';
 
@@ -85,6 +69,7 @@ class MysqlConnection {
 				return false;
 			}
 			EventLog::info("Selected [{$this->_db_name}]");
+			mysql_set_charset('utf8', $this->_connection);
 		}
 		return true;
 	}
