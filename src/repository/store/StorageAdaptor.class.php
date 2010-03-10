a4c9de5c7f44a0d4da7786bb3731d5a593e34f00
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2010-03-10 00:47:05 +0000

    changing StorageAdaptor to use default table from passed in query object
    git-svn-id: http://floe.googlecode.com/svn/trunk@360 bff8106b-f335-0410-b21d-6d730982856d

--- src/repository/store/StorageAdaptor.class.php
+++ src/repository/store/StorageAdaptor.class.php
@@ -1,32 +1,16 @@
 <?php
 /**
+ * This file is part of Floe, a graceful PHP framework.
+ * Copyright (C) 2005-2010 Mark Rickerby <http://maetl.net>
+ *
+ * See the LICENSE file distributed with this software for full copyright, disclaimer
+ * of liability, and the specific limitations that govern the use of this software.
+ *
  * $Id$
  * @package repository
  * @subpackage store
- *
- * Copyright (c) 2007-2009 Coretxt
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
  */
+
 require_once 'mysql/MysqlConnection.class.php';
 require_once 'mysql/MysqlGateway.class.php';
 require_once 'mysql/MysqlQuery.class.php';
@@ -176,6 +160,7 @@ class StorageAdaptor {
 		 * @todo extract to SqlAdaptor interface
 		 */
 		function query($statement) {
+			$this->gateway->currentTable = 'entries';
 			return $this->gateway->query($statement);
 		}
 
