ad1de88904b1be8526b9ee674f08a4a17bd54b8f
coretxt <coretxt@bff8106b-f335-0410-b21d-6d730982856d>
2010-03-12 12:12:09 +0000

    separating tests for PhpTemplate handler
    git-svn-id: http://floe.googlecode.com/svn/trunk@365 bff8106b-f335-0410-b21d-6d730982856d

---
+++ dev/tests/template.test.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'simpletest/autorun.php';
+require_once dirname(__FILE__).'/../../src/server/template/PhpTemplate.class.php';
+
+if (!defined('TPL_DIR')) define('TPL_DIR', dirname(__FILE__).'/resources/templates/');
+
+class TemplateTest extends UnitTestCase {
+	
+	function testTemplateRenderPlain() {
+		$template = new PhpTemplate();
+		$body = $template->render('hello');
+		$this->assertPattern("/<h1>Hello World<\/h1>/", $body);
+	}
+	
+	function testTemplateRenderVars() {
+		$template = new PhpTemplate();
+		$template->assign('booleanVar', true);
+		$template->assign('integerVar', 999);
+		$template->assign('stringVar', 'sesame');
+		$template->assign('arrayVar', array("green","eggs","boiled","ham"));
+		$object = new stdClass; $object->var = "value";
+		$template->assign('objectVar', $object);
+		$body = $template->render('vars');
+		$this->assertPattern("/<li>booleanVar: 1<\/li>/", $body);
+		$this->assertPattern("/<li>integerVar: 999<\/li>/", $body);
+		$this->assertPattern("/<li>stringVar: sesame<\/li>/", $body);
+		$this->assertPattern("/<li>arrayVar: green,eggs,boiled,ham<\/li>/", $body);
+		$this->assertPattern("/<li>objectVar: value<\/li>/", $body);
+	}
+	
+	function testTemplateRenderWrappedLayout() {
+		$template = new PhpTemplate();
+		$template->wrap('wrapper');
+		$template->assign('foo', 'bar');
+		$body = $template->render('hello');
+		$this->assertPattern("/<span>bar<\/span>/", $body);
+		$this->assertPattern("/<div><h1>Hello World<\/h1><\/div>/", $body);
+	}
+	
+	function testTemplateRenderEmbeddedSubTemplate() {
+		$template = new PhpTemplate();
+		$template->assign('foo', 'parent');
+		$template->assign('bar', 'child');
+		$body = $template->render('parent');
+		$this->assertPattern("/<span>parent<\/span>/", $body);
+		$this->assertPattern("/<span>child<\/span>/", $body);
+		$this->assertPattern("/<h1>Hello World<\/h1>/", $body);
+	}
+	
+}
+
+?>
