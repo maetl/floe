07f8c1764e660dee4eb4923f5e1939f0d05f4fe0
maetl <me@maetl.net>
2011-05-01 00:12:09 +1000

    adding autoload and classpath manifest generator

--- src/init.php
+++ src/init.php
@@ -9,75 +9,36 @@
  * @package floe
  */
 
-require_once 'framework/Package.class.php';
-require_once 'framework/Using.class.php';
-require_once 'server/Membrane.class.php';
+require_once 'framework/Environment.class.php';
 
 /**
  * Load the application defaults.
  * @package floe
  */
-class FloeApp {
+class Floe {
 	
 	const VERSION = '0.6.6';
+	const APP_DIR = 'app';
+	const DEV_DIR = 'dev';
+	const LIB_DIR = 'lib';
+	const TMP_DIR = 'tmp';
+	const WEB_DIR = 'www';
+	const CTR_DIR = 'controllers';
+	const MOD_DIR = 'models';
+	const TPL_DIR = 'templates';
 	
 	/**
-	 * Load the application config.
+	 * Register the application environment.
 	 *
-	 * Optional parameters points to where class packages are installed,
-	 * otherwise defaults to the local "/../app/" directory.
-	 *
-	 * @param $app path to app directory
+	 * @param string $path path to the application root.
 	 */
-	public static function register($app = false) {
-		if (!$app) $app = dirname(__FILE__).'/../app/';
-		$floePath = dirname(__FILE__);
+	public static function init($path) {
 		
-		try {
-			if (!require_once $app.'/config.php')
-				throw new Exception("No application configured.");
+		Environment::setClassPath(dirname(__FILE__));
+		Environment::setClassPath($path);
 
-			if (!defined('FloeApp_Controllers')) define('FloeApp_Controllers', $app.'/controllers');
-			if (!defined('FloeApp_Models')) define('FloeApp_Models', $app.'/models');
-			if (!defined('FloeApp_Locales')) define('FloeApp_Locales', $app.'/locales');
-			if (!defined('FloeApp_Templates')) define('FloeApp_Templates', $app.'/templates');
-			if (!defined('FloeApp_Scratch')) define('FloeApp_Scratch', sys_get_temp_dir());
+		Environment::buildManifest();
 
-			foreach(array($floePath.'/server/receptors/',
-						  $floePath.'/server/controllers/',
-						  $floePath.'/repository/',
-			 			  FloeApp_Controllers,
-			 			  FloeApp_Models) as $classPath) {
-				set_include_path(get_include_path().PATH_SEPARATOR.$classPath);
-			}
-
-			spl_autoload_extensions('.class.php, .php, .model.php, .inc');
-
-			spl_autoload_register();
-			spl_autoload_register("FloeApp_Helpers_Autoload");
-
-			if (!require_once $app.'/controllers/application.controller.php')
-				throw new Exception("No application configured.");	
-		
-		} catch(Exception $error) {
-			$response = new Response();
-			$response->raise($error);
-			$response->out();
-			exit;
-		}
-	}
-}
-
-/**
- * Structured search for application specific helper objects.
- * Hook for spl_autoload_register
- * @todo flexible naming conventions
- */
-function FloeApp_Helpers_Autoload($className) {
-	if (strstr($className, 'Finder')) {
-		$classPath = str_replace('finder', '.finder.php', strtolower($className));
-		require_once FloeApp_Models."/$classPath";
-	} elseif (file_exists(FloeApp_Models."/$className.model.php")) {
-		require_once FloeApp_Models."/$className.model.php";
+		spl_autoload_register(array('Environment', 'loadClass'));
 	}
 }
